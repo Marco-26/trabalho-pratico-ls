1feda30e86031e9b29913ba4b393348ade543fe5
Marco <markcostah@gmail.com>
2022-05-18 13:34:48 +0100

    grid modular

--- src/components/grid/grid.jsx
+++ src/components/grid/grid.jsx
@@ -2,62 +2,65 @@ import React,{ useState } from "react";
 import '../reset.css';
 import './grid.css';
 import { data } from "../data"
+import styled from "styled-components"
 
 const Grid = (props) => {
     const {level} = props;
-    const [cells] = useState(Array.from({ length : 64 }))
     
+    const cellsArray = []
+    let cellQuantity, cssRepeat;    
+
     const shuffle = arr => [...arr].sort(() => Math.random() - 0.5);
     const newData = shuffle(data);
 
-    // dependendo da dificuldade, o jogo apenas tem um certo numero de palavras para serem descobertas
+    // dependendo da dificuldade, o jogo apenas tem um certo numero de palavras e um certo numero de cells
+    // para serem descobertas
+
     let wordAmount;
 
     switch(level){
         case(0):
-        wordAmount = 0;
+            wordAmount = 0;
+            cellQuantity = 0;
+            cssRepeat = 0;
             break;
         case(1):
-        wordAmount=3;
+            wordAmount=3;
+            cellQuantity = 64;
+            cssRepeat = 8;
             break;
         case(2):
-        wordAmount = 5;
+            wordAmount = 5;
+            cellQuantity = 81;
+            cssRepeat = 9;
             break;
         case(3):
-        wordAmount = 7;
+            wordAmount = 7;
+            cellQuantity = 100;
+            cssRepeat = 10;
             break;
+    }
+    
+    for(let i=0; i<cellQuantity; i++){
+        cellsArray.push(null);
     }
     
     const words = [];
 
     for (let index = 0; index < wordAmount; index++) {
         words.push(newData[index]);      
-    }
+    }      
 
-    function randomChar() {
-        let char = '';
-        let characters = 'abcdefghijklmnopqrstuvwxyz';
-        char = characters.charAt(Math.floor(Math.random()*characters.length));
-        return char;
-    }     
-    
-    // const namesArray = ["Rodrigo", "Alex"];
-
-    // namesArray.map(function (name) {
-    //     name.split('').forEach(function (char){
-    //         return (
-    //         <div>
-    //             <p>Hello {char}</p>
-    //         </div>)
-    //     }) 
-    // })
+    const Grid = styled.div`
+        display: grid;
+        grid-template-columns: repeat(${cssRepeat}, 30px);
+    `;
 
     return(
         <div className="grid-container">
-            <div className="grid">
-                {cells.map(() => <div className="cell">{randomChar()}</div>)}
-                {/* {namesArray.map((name) => name.split('').forEach((char) => <div>{char}</div>))} */}
-            </div>
+            <Grid>
+                {cellsArray.map(() => <div className="cell">{randomChar()}</div>)}
+            </Grid>
             <div className="words">
                 {words.map((word) => (
                     <div>
@@ -66,8 +69,14 @@ const Grid = (props) => {
                 ))}
             </div>
         </div>
-    );
-    
+    );  
 }
+
+function randomChar() {
+    let char = '';
+    let characters = 'abcdefghijklmnopqrstuvwxyz';
+    char = characters.charAt(Math.floor(Math.random()*characters.length));
+    return char;
+}  
 
 export default Grid;
