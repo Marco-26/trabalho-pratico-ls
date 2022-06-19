e2fbffe0a8b44e5070e22c4d5b9dfd71d6356a26
Marco <markcostah@gmail.com>
2022-06-19 14:13:42 +0100

    update

---
+++ src/Util.js
@@ -0,0 +1,272 @@
+function gridtemp(cssRepeat, palavra) {
+  let mapa = [];
+
+  let tempGrid = new Array(cssRepeat);
+  for (let i = 0; i < cssRepeat; i++) {
+    tempGrid[i] = new Array(cssRepeat);
+    for (let j = 0; j < cssRepeat; j++) {
+      tempGrid[i][j] = 0;
+    }
+  }
+
+  //por as palavras
+
+  let tamanho, x, y, direçao;
+  let local = [];
+  let wordpose = [];
+
+  for (let j = 0; j < palavra.length; j++) {
+    tamanho = palavra[j].length;
+
+    let espasso = false;
+    let reverse = false;
+
+    local = [];
+
+    do {
+      x = RandomN(cssRepeat);
+      y = RandomN(cssRepeat);
+
+      direçao = RandomN(7);
+
+      let k, l;
+
+      switch (direçao) {
+        case 0: //cima
+          if (tamanho < x) {
+            for (k = x; k > x - tamanho; k--) {
+              if (tempGrid[k - 1][y] === 0) {
+                espasso = true;
+              } else {
+                espasso = false;
+                break;
+              }
+            }
+          }
+          break;
+        case 1: //baixo
+          if (tamanho < x) {
+            for (k = x; k > x - tamanho; k--) {
+              if (tempGrid[k - 1][y] === 0) {
+                espasso = true;
+                reverse = true;
+              } else {
+                reverse = false;
+                espasso = false;
+                break;
+              }
+            }
+          }
+          break;
+        case 2: //direita
+          if (tamanho < y) {
+            for (k = y; k > y - tamanho; k--) {
+              if (tempGrid[x][k - 1] === 0) {
+                espasso = true;
+              } else {
+                espasso = false;
+                break;
+              }
+            }
+          }
+          break;
+        case 3: //esquerda
+          if (tamanho < y) {
+            for (k = y; k > y - tamanho; k--) {
+              if (tempGrid[x][k - 1] === 0) {
+                espasso = true;
+                reverse = true;
+              } else {
+                reverse = false;
+                espasso = false;
+                break;
+              }
+            }
+          }
+          break;
+        case 4: //dig ed
+          if (tamanho < x && tamanho < y) {
+            for (k = x, l = y; k > x - tamanho && l > y - tamanho; k--, l--) {
+              if (tempGrid[k - 1][l - 1] === 0) {
+                espasso = true;
+              } else {
+                espasso = false;
+                break;
+              }
+            }
+          }
+          break;
+        case 5:
+          if (tamanho < x && tamanho < y) {
+            for (k = x, l = y; k > x - tamanho && l > y - tamanho; k--, l--) {
+              if (tempGrid[k - 1][l - 1] === 0) {
+                espasso = true;
+                reverse = true;
+              } else {
+                reverse = true;
+                espasso = false;
+                break;
+              }
+            }
+          }
+          break;
+
+        case 6:
+          if (tamanho < x && tamanho < cssRepeat - y) {
+            for (k = x, l = cssRepeat - y; k > x - tamanho; k--, l++) {
+              if (tempGrid[k - 1][l - 1] === 0) {
+                espasso = true;
+              } else {
+                espasso = false;
+                break;
+              }
+
+              espasso = true;
+            }
+          }
+
+          break;
+
+        case 7:
+          if (tamanho < x && tamanho < cssRepeat - y) {
+            for (k = x, l = cssRepeat - y; k > x - tamanho; k--, l++) {
+              if (tempGrid[k - 1][l - 1] === 0) {
+                espasso = true;
+                reverse = true;
+              } else {
+                reverse = false;
+                espasso = false;
+                break;
+              }
+
+              espasso = true;
+            }
+          }
+
+          break;
+
+        default:
+          break;
+      }
+
+      if (espasso == true) {
+        if (reverse == true) {
+          let letratempo = palavra[j].split("").reverse().join("");
+
+          switch (direçao) {
+            case 1:
+              for (let i = 0, k = x; k > x - tamanho; k--, i++) {
+                tempGrid[k - 1][y] = letratempo[i].toUpperCase();
+                local[i] = `${k - 1}-${y}`;
+              }
+              break;
+            case 3:
+              for (let i = 0, k = y; k > y - tamanho; k--, i++) {
+                tempGrid[x][k - 1] = letratempo[i].toUpperCase();
+                local[i] = `${x}-${k - 1}`;
+              }
+              break;
+            case 5:
+              for (
+                let i = 0, k = x, l = y;
+                k > x - tamanho && l > y - tamanho;
+                k--, l--, i++
+              ) {
+                tempGrid[k - 1][l - 1] = letratempo[i].toUpperCase();
+                local[i] = `${k - 1}-${l - 1}`;
+              }
+              break;
+            case 7:
+              for (
+                let i = 0, k = x, l = cssRepeat - y;
+                k > x - tamanho;
+                k--, l++, i++
+              ) {
+                tempGrid[k - 1][l - 1] = letratempo[i].toUpperCase();
+                local[i] = `${k - 1}-${l - 1}`;
+              }
+              break;
+            default:
+              break;
+          }
+        } else {
+          switch (direçao) {
+            case 0:
+              for (let i = 0, k = x; k > x - tamanho; k--, i++) {
+                tempGrid[k - 1][y] = palavra[j][i].toUpperCase();
+                local[i] = `${k - 1}-${y}`;
+              }
+              break;
+            case 2:
+              for (let i = 0, k = y; k > y - tamanho; k--, i++) {
+                tempGrid[x][k - 1] = palavra[j][i].toUpperCase();
+                local[i] = `${x}-${k - 1}`;
+              }
+
+              break;
+            case 4:
+              for (
+                let i = 0, k = x, l = y;
+                k > x - tamanho && l > y - tamanho;
+                k--, l--, i++
+              ) {
+                tempGrid[k - 1][l - 1] = palavra[j][i].toUpperCase();
+                local[i] = `${k - 1}-${l - 1}`;
+              }
+
+              break;
+            case 6:
+              for (
+                let i = 0, k = x, l = cssRepeat - y;
+                k > x - tamanho;
+                k--, l++, i++
+              ) {
+                tempGrid[k - 1][l - 1] = palavra[j][i].toUpperCase();
+                local[i] = `${k - 1}-${l - 1}`;
+              }
+              break;
+            default:
+              break;
+          }
+        }
+      }
+    } while (espasso == false);
+    wordpose.push(local);
+  }
+
+  //por letras onde n tem
+  for (let i = 0; i < cssRepeat; i++) {
+    for (let j = 0; j < cssRepeat; j++) {
+      if (tempGrid[i][j] === 0) {
+        //tempGrid[i][j] = randomChar();
+      }
+    }
+  }
+
+  //passar de bi para uni
+  let k = 0;
+  for (let i = 0; i < cssRepeat; i++) {
+    for (let j = 0; j < cssRepeat; j++, k++) {
+      mapa[k] = {
+        nome: tempGrid[i][j],
+        pos: `${i}-${j}`,
+        clicked: false,
+      };
+    }
+  }
+
+  return { mapa, wordpose };
+}
+
+function RandomN(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function randomChar() {
+  let char = "";
+  let characters = "abcdefghijklmnopqrstuvwxyz";
+  char = characters.charAt(Math.floor(Math.random() * characters.length));
+  return char.toUpperCase();
+}
+
+export default gridtemp;
--- src/components/grid/grid.jsx
+++ src/components/grid/grid.jsx
@@ -1,96 +1,216 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../reset.css";
 import "./grid.css";
-import { data } from "../data";
 import { Board } from "./board";
+import Leters from "./leters";
+
+let wordsFound = 0;
 
 const Grid = (props) => {
-  const { cells, level } = props;
-  const [grid, setGrid] = useState(cells);
+  const { mapa, palavra, cssRepeat, wordpose, wordAmount,setTrigger,handleGameStart,setWin} = props;
+  const [grid, setGrid] = useState(mapa);
+  const [posicao, setposicao] = useState([]);
+  const [tecla, settecla] = useState([]);
+  const [words, setwords] = useState(palavra);
+  const [verificou, setverificou] = useState([0]);
 
-  let cssRepeat, wordAmount;
+  useEffect(() => {
+    if (posicao.length >= 2) {
+      setposicao([]);
+      settecla([]);
+    }
+  });
 
-  switch (level) {
-    case 0:
-      wordAmount = 0;
-      cssRepeat = 0;
-      break;
-    case 1:
-      wordAmount = 3;
-      cssRepeat = 8;
-      break;
-    case 2:
-      wordAmount = 4;
-      cssRepeat = 9;
-      break;
-    case 3:
-      wordAmount = 5;
-      cssRepeat = 10;
-      break;
-    default:
-      break;
-  }
+  const addClick = (key) => {
+    if (grid[key].clicked == false) {
+      grid[key] = {
+        nome: grid[key].nome,
+        pos: grid[key].pos,
+        clicked: true,
+      };
+    }
+  };
 
-  let words = getData(wordAmount);
+  const removeClick = (key) => {
+    if (grid[key].clicked == true) {
+      grid[key] = {
+        nome: grid[key].nome,
+        pos: grid[key].pos,
+        clicked: false,
+      };
+    }
+  };
 
   const handleClick = (key) => {
+    if (posicao.length > 2) {
+      setposicao([]);
+    }
     setGrid(
       grid.map((cell, i) => {
         if (i === key) {
-          return {
-            clicked: true,
-          };
+          if (cell.clicked == false) {
+            posicao.push(cell.pos);
+
+            tecla.push(key);
+
+            tecla.sort(function (a, b) {
+              return a - b;
+            });
+
+            return {
+              nome: cell.nome,
+              pos: cell.pos,
+              clicked: true,
+            };
+          }
         }
         return cell;
       })
     );
   };
 
+  function verificaPalavra() {
+    for (let j = 0; j < wordpose.length; j++) { 
+      if (wordpose[j].length == posicao.length) {
+        if (
+          JSON.stringify(wordpose[j].sort()) === JSON.stringify(posicao.sort())
+        ) {
+          let a = (verificou[0]+Math.floor(Math.random() * 6)+1*palavra[j].palavra.length)
+          verificou.shift()
+          verificou.push(a)        
+          
+          words[j].clicked = true;
+          wordsFound++;
+          if(wordsFound === wordAmount){
+            setTrigger(true);
+            handleGameStart();
+            setWin(true);
+            wordsFound = 0;
+          }
+          return true;
+        }
+      }
+    }
+  }
+
+  posicao.sort();
+  
+  if (posicao.length == 2) {
+    posicao.sort();
+    let entrou = false;
+
+    if (posicao[0][0] == posicao[1][0]) {
+      for (let i = tecla[0] + 1; i < tecla[1]; i++) {
+        posicao.push(grid[i].pos);
+        tecla.push(i);
+      }
+
+      if (verificaPalavra() == true) {
+        entrou = true;
+        for (let i = tecla[0]; i < tecla[1]; i++) {
+          addClick(i);
+        }
+      }
+    }
+
+    if (posicao[0][2] == posicao[1][2]) {
+      let i = tecla[0];
+
+      do {
+        i = i + cssRepeat;
+        posicao.push(grid[i].pos);
+        tecla.push(i);
+      } while (i < tecla[1] - cssRepeat);
+
+      if (verificaPalavra() == true) {
+        entrou = true;
+        let i = tecla[0];
+        do {
+          i = i + cssRepeat;
+          addClick(i);
+        } while (i < tecla[1] - cssRepeat);
+      }
+    }
+
+    let arrei = posicao;
+    let ar = [];
+    arrei.forEach((element) => {
+      let num = element.split("-");
+      ar.push(parseInt(num[0]));
+      ar.push(parseInt(num[1]));
+    });
+
+    if (ar[2] - ar[0] == ar[3] - ar[1]) {
+      let i = tecla[0];
+      do {
+        i = i + cssRepeat + 1;
+        posicao.push(grid[i].pos);
+        tecla.push(i);
+      } while (i < tecla[1] - 2 * cssRepeat);
+
+      if (verificaPalavra() == true) {
+        entrou = true;
+        let i = tecla[0];
+        do {
+          i = i + cssRepeat + 1;
+          addClick(i);
+        } while (i < tecla[1] - cssRepeat);
+      }
+    }
+
+    if (ar[2] - ar[1] == ar[0] - ar[3]) {
+      let i = tecla[0];
+      do {
+        i = i + cssRepeat - 1;
+        posicao.push(grid[i].pos);
+        tecla.push(i);
+      } while (i < tecla[1] - cssRepeat);
+
+      if (verificaPalavra() == true) {
+        entrou = true;
+        let i = tecla[0];
+        do {
+          i = i + cssRepeat - 1;
+          addClick(i);
+        } while (i < tecla[1]);
+      }
+    }
+
+    if (entrou == false) {
+      removeClick(tecla[0]);
+      removeClick(tecla[1]);
+    }
+  }
+
   return (
-    <div className="grid-container">
-      <Board cssRepeat={cssRepeat}>
-        {grid.map((cell, i) => (
-          <div
-            key={i}
-            className="cell"
-            onClick={() => {
-              handleClick(i);
-            }}
-            style={{ backgroundColor: cell.clicked ? "yellow" : "white" }}
-          >
-            {randomChar()}
-          </div>
-        ))}
-      </Board>
-      <div className="words">
-        {words.map((word) => (
-          <div>
-            <p>{word}</p>
-          </div>
-        ))}
+    <div>
+      <p className="points">Pontuação: {verificou[0]}</p>
+      <div className="grid-container">
+        <br></br>
+        <Board cssRepeat={cssRepeat}>
+          {grid.map((cell, i) => (
+            <Leters
+              letra={cell.nome}
+              i={i}
+              handleClick={handleClick}
+              clicked={cell.clicked}
+            />
+          ))}
+        </Board>
+        <div className="words">
+          {words.map((word) => (
+            <div
+              style={{
+                backgroundColor: word.clicked ? "rgb(100, 2300, 136)" : "red",
+              }}
+            >
+              <p>{word.palavra}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
 };
-
-function getData(wordAmount) {
-  const shuffle = (arr) => [...arr].sort(() => Math.random() - 0.5);
-  const newData = shuffle(data);
-
-  const words = [];
-
-  for (let index = 0; index < wordAmount; index++) {
-    words.push(newData[index]);
-  }
-
-  return words;
-}
-
-function randomChar() {
-  let char = "";
-  let characters = "abcdefghijklmnopqrstuvwxyz";
-  char = characters.charAt(Math.floor(Math.random() * characters.length));
-  return char;
-}
 
 export default Grid;
